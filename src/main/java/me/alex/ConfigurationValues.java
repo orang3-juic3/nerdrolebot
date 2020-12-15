34774cd46dc8a93bae45469def20b755f9796039
Alex <alexzatstone@gmail.com>
2020-12-15 14:29:42 +0000

    ConfigurationValues is functional now?

--- src/main/java/me/alex/ConfigurationValues.java
+++ src/main/java/me/alex/ConfigurationValues.java
@@ -1,5 +1,7 @@
 package me.alex;
 
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.nio.file.Paths;
 
@@ -13,9 +15,33 @@ public class ConfigurationValues {
         try {
             String workingDir = Paths.get("").toAbsolutePath().toString();
             File f = new File(workingDir + "\\conf.json");
-            BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(f));
+            if (f.createNewFile()) {
+                BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(f));
+                exemptionList = new long[]{266279893543682049L, 230336110637744131L};
+                weeksOfData = 2;
+                roleId = 706554375572684860L;
+                serverId = 679434326282207238L;
+                Gson gson = new Gson();
+                bufferedWriter.write(gson.toJson(this));
+                throw new InvalidConfigurationException("Created new configuration file, please edit before restarting the program!");
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
+            String line;
+            StringBuilder config = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                config.append(line);
+            }
+            Gson gson = new Gson();
+            gson.fromJson(config.toString(), this.getClass());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static synchronized ConfigurationValues getInstance(){
+        if(instance == null){
+            instance = new ConfigurationValues();
+        }
+        return instance;
     }
 }
--- src/main/java/me/alex/Main.java
+++ src/main/java/me/alex/Main.java
@@ -21,6 +21,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             firstTimeDatabaseSetup();
+            ConfigurationValues configurationValues = ConfigurationValues.getInstance();
             JDA jda;
             JDABuilder jdaBuilder = JDABuilder.createDefault("token");
             jda = jdaBuilder.build();
